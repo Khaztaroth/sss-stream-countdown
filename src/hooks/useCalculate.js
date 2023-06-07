512b9f33677313d5ad8dee649af0bf76568532ba
khaztaroth <astaroth503@gmail.com>
2023-06-07 17:03:41 -0400

    separated calculator from formatting func

---
+++ src/hooks/useCalculate.js
@@ -0,0 +1,52 @@
+import {useEffect, useMemo, useState} from 'react'
+
+const { DateTime } = require('luxon');
+
+export default function Calculator(){
+
+const inNY = {zone: "America/New_York"}
+const [nowInNY, setNowInNY] = useState(DateTime.local(inNY));
+
+const startOfWeek = nowInNY.startOf('week');
+const days = {wed: 3, sun: 7}
+const daysToWED = (days.wed - startOfWeek.weekday + 7 ) %7;
+const daysToSUN = (days.sun- startOfWeek.weekday + 7 ) %7;
+
+const nextWED = startOfWeek.plus({days: daysToWED});
+const nextSUN = startOfWeek.plus({days: daysToSUN});
+
+const nextWedDate = DateTime.fromFormat(`${nextWED.month}/${nextWED.day}/${nextWED.year}, 9:00 PM`, 'f', inNY);
+const nextSunDate = DateTime.fromFormat(`${nextSUN.month}/${nextSUN.day}/${nextSUN.year}, 9:00 PM`, 'f', inNY);
+const specialStream = DateTime.fromFormat(`06/07/2023, 7:00 PM`, 'f', inNY);
+
+const timeUntilWedStream = nextWedDate.diff(nowInNY, ['days', 'hours', 'minutes', 'seconds'])
+const timeUntilSunStream = nextSunDate.diff(nowInNY, ['days', 'hours', 'minutes', 'seconds'])
+const timeUntiolSpecialStream = specialStream.diff(nowInNY, ['days', 'hours', 'minutes', 'seconds'])
+
+
+const nextRegularStream = useMemo(() => {
+    if (timeUntilWedStream.hours >= -2) {
+        return timeUntilWedStream
+    } else {
+        return timeUntilSunStream
+    }
+})
+
+const timeDiff = useMemo(() => {
+    if (timeUntiolSpecialStream.hours >= -4) {
+        return timeUntiolSpecialStream
+    } else {
+        return nextRegularStream
+    }
+})
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        setNowInNY(DateTime.local(inNY));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+    return timeDiff
+}
---
+++ src/hooks/useTimer.js
@@ -0,0 +1,39 @@
+import Calculator from "./useCalculate"
+
+export const timer = () => {
+    const timeDiff = Calculator()
+
+    if (timeDiff.days > 0) {
+        return (
+            timeDiff.toFormat(
+                `d'${timeDiff.days === 1 ? 'day' : 'days'}'
+                h'${timeDiff.hours === 1 ? 'hour' : 'hours'}'
+                m'${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'
+                s'${timeDiff.seconds < 2 ? 'second' : 'seconds'}' `
+                )
+        )
+    } else if (timeDiff.hours > 0) {
+        return (
+            timeDiff.toFormat(
+                `h'${timeDiff.hours === 1 ? 'hour' : 'hours'}'
+                m'${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'
+                s'${timeDiff.seconds < 2 ? 'second' : 'seconds'}' `
+                )
+        )
+    } else if (timeDiff.minutes > 0) {
+        return (
+            timeDiff.toFormat(
+                `m'${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'
+                s'${timeDiff.seconds < 2 ? 'second' : 'seconds'}' `
+                )
+        )
+    } else if (timeDiff.seconds > 0) {
+        return (
+            timeDiff.toFormat(
+                `s'${timeDiff.seconds < 1 ? 'second' : 'seconds'}' `
+                )
+        )
+    } else {
+     return ('Stream happening now!')
+    }
+}
